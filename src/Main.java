d0f62a15c62506e5f217d6f9da710d00da97100d
Suzanne <shlindel@haverford.edu>
2023-04-28 12:56:51 -0400

    trying out github

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main (String[] args){
         System.out.println("Successfuly created a new Project");
+        System.out.println("Successfully shared project on github");
     }
 }
